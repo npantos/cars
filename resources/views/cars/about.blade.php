fcc5be65d58cc45de44d5376c29294a06cef473c
Nemanja Pantoš <npantos@gmail.com>
2017-12-21 21:03:24 +0100

    small fixes

--- resources/views/cars/about.blade.php
+++ resources/views/cars/about.blade.php
@@ -5,7 +5,11 @@
 @endsection
 
 @section('content')
+    <div class="jumbotron">
     <h1>About us</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci autem beatae corporis deleniti dolor eveniet hic illo libero, numquam porro quasi quia quis quisquam quos rem temporibus unde! Cupiditate, doloremque?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci autem beatae corporis deleniti dolor eveniet hic illo libero, numquam porro quasi quia quis quisquam quos rem temporibus unde! Cupiditate, doloremque?</p>
+    </div>
 @endsection
 
 
--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -5,9 +5,29 @@
 @endsection
 
 @section('content')
-    @foreach ($cars as $car)
-        <li><a href="{{ route('car',['id'=>$car->id]) }}">{{$car->title}}</a></li>
-    @endforeach
+    <h1>List of all cars</h1>
+    <table class="table table-hover">
+        <thead>
+        <tr class="table-success">
+            <th scope="col">Id</th>
+            <th scope="col">Name</th>
+            <th scope="col">Producer</th>
+            <th scope="col">Number of doors</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @foreach ($cars as $car)
+            <tr class="table-active">
+                <th scope="row">{{$car->id}}</th>
+                <td><a href="{{ route('car',['id'=>$car->id]) }}">{{$car->title}}</a></td>
+                <td>{{$car->producer}}</td>
+                <td>{{$car->number_of_doors}}</td>
+            </tr>
+        @endforeach
+
+        </tbody>
+    </table>
 @endsection
 
 
